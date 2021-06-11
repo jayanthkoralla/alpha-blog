f36d702caa99fbb744a34e791540a73b12b1f954
Jayanth <jayanth.koralla@gmail.com>
2021-06-11 12:38:18 +0530

    Add one-to-many association between users and articles from back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@ class ArticlesController < ApplicationController
   end
   def create
     @article=Article.new(params.require(:article).permit(:title,:description))
+    @article.user_id = User.first.id
     @article.save
     if(@article.save)
       redirect_to @article
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 4,maximun: 14}
   validates :description , presence: true, length: {minimum: 4,maximun: 14}
 end
