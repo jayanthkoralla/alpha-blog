fcf5ba88f64d221e2c1939766e16be49332bd6eb
Jayanth <jayanth.koralla@gmail.com>
2021-06-14 16:57:28 +0530

    Add/edit/destroy users and restricted buttons to particlar users(logged in)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :require_user, except: [:index,:show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def index 
     @article=Article.all
   end
@@ -7,7 +9,7 @@ class ArticlesController < ApplicationController
   end
   def create
     @article=Article.new(params.require(:article).permit(:title,:description))
-    @article.user_id = User.last.id
+    @article.user_id = current_user.id
     @article.save
     if(@article.save)
       redirect_to @article
@@ -35,3 +37,10 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 end
+private
+  def require_same_user
+    if current_user.id != @article.user
+      flash[:alert]="You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,48 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show,:edit,:update,:destroy]
+  before_action :require_user, only: [:edit, :update, :destroy ]
+  def show
+    @article=@user.articles
+  end 
+  def index
+    @user=User.all
+  end
   def new
     @user=User.new
   end
   def create
-    @user=User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice]="Welcome to Alpha Blog"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'new'
     end
   end
   def edit
-    @user=User.find(params[:id])
   end
   def update
     @user=User.find(params[:id])
     if @user.update(user_params)
       flash[:notice]="Your account info successfullt updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
+  end
+  def destroy
+    @user.destroy
+    session[:user_id]=nil
+    flash[:notice]="Account and all associated articles successfully deleted"
+    redirect_to articles_path
   end
   private
   def user_params
     params.require(:user).permit(:username,:email,:password)
   end
+
+  def set_user
+    @user=User.find(params[:id])
+  end
+
 end
